985ce65270af78f104993d4ef0b794afbdb7aa37
Eason_Hsu <hsunslife@gmail.com>
2017-06-02 18:47:18 +0800

    MVVM建立

---
+++ DigiKey/Models/MainWindowModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DigiKey.Models
+{
+    public class MainWindowModel
+    {
+        /// <summary>
+        /// 診所伺服器位置
+        /// </summary>
+        public string ServerIP { get; set; }
+        /// <summary>
+        /// 機構代碼
+        /// </summary>
+        public string AgencylID { get; set; }
+        /// <summary>
+        /// 驗證碼
+        /// </summary>
+        public string VerificationCode { get; set; }
+    }
+}
---
+++ DigiKey/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DigiKey.Models;
+
+namespace DigiKey.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        private MainWindowModel mainWindowModel;
+        public MainWindowViewModel()
+        {
+            mainWindowModel = new MainWindowModel();
+        }
+        
+        public string ServerIP
+        {
+            get { return mainWindowModel.ServerIP; }
+            set { mainWindowModel.ServerIP = value; }
+        }
+
+        public string AgencyID
+        {
+            get { return mainWindowModel.AgencylID; }
+            set { mainWindowModel.AgencylID = value; }
+        }
+
+        public string VerificationCode
+        {
+            get { return mainWindowModel.VerificationCode; }
+            set { mainWindowModel.VerificationCode = value; }
+        }
+    }
+}
