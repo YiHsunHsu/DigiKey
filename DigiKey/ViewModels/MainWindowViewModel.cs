2909095e851b01ee177ef92565ce96ea91208890
EasonHsu <justshee@hotmail.com>
2017-06-07 17:33:21 +0800

    新增Class,ViewModelBase,Command 驗證碼驗證

--- DigiKey/ViewModels/MainWindowViewModel.cs
+++ DigiKey/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,21 @@
-﻿using DigiKey.Models;
+﻿using DigiKey.Class;
+using DigiKey.Commands;
+using DigiKey.Models;
 using DigiKey.ViewModelBases;
 using System.Data.SqlClient;
+using System.Windows.Input;
 
 namespace DigiKey.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         private MainWindowModel mainWindowModel;
+        private KeyGenerator keyGenerator;
+        private string _PrivateKey;
         public MainWindowViewModel()
         {
             mainWindowModel = new MainWindowModel();
+            keyGenerator = new KeyGenerator();
         }
         
         public string ServerIP
@@ -25,15 +31,50 @@ namespace DigiKey.ViewModels
         public string AgencyCode
         {
             get { return mainWindowModel.AgencyCode; }
-            set { mainWindowModel.AgencyCode = value; }
+            set
+            {
+                mainWindowModel.AgencyCode = value;
+                _PrivateKey = keyGenerator.SHA384Encode(mainWindowModel.AgencyCode);
+                OnPropertyChanged("AgencyCode");
+            }
         }
 
         public string VerificationCode
         {
             get { return mainWindowModel.VerificationCode; }
-            set { mainWindowModel.VerificationCode = value; }
+            set
+            {
+                mainWindowModel.VerificationCode = value;
+                OnPropertyChanged("VerificationCode");
+            }
         }
 
+        private RelayCommand _register;
+        public ICommand RegisterToServer
+        {
+            get
+            {
+                if (_register == null)
+                {
+                    _register = new RelayCommand(Register, CanRegister);
+                }
+                return _register;
+            }
+        }
+
+        private void Register()
+        {
+        }
+
+        private bool CanRegister()
+        {
+           if ((!string.IsNullOrEmpty(AgencyCode)) || (!string.IsNullOrEmpty(VerificationCode)))
+            {
+                if(!string.IsNullOrEmpty(VerificationCode))
+                    return VerificationCode.Equals(_PrivateKey) ? true : false;
+            }
+            return false;
+        }
         //private bool checkCon(string ip)
         //{
         //    SqlConnection con = new SqlConnection(@"Server=" + ip + @"\DIGIDENTAL;Database=DigiDental;User Id=sa;Password=0939566880;");
